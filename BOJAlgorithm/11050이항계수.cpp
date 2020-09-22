226b93778e97bebacf6a6b1e48f0585308fb0dc5
SuYoung <golborneville@gmail.com>
2020-09-22 22:05:59 +0900

    11050 이항계수

---
+++ BOJAlgorithm/11050이항계수.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int n, k;
+int fib[23];
+int main() {
+	std::cin >> n >> k;
+	fib[0] = 1;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		fib[i] = fib[i - 1] * i;
+	}
+	
+	int  re = (fib[n] / fib[k]) / fib[n-k];
+	std::cout << re;
+	return 0;
+}
