445efd62df87ef3be6a03b0c5e1a1072a912f2a6
SuYoung <golborneville@gmail.com>
2020-12-30 23:37:31 +0900

    7567그릇

---
+++ BOJAlgorithm/7567그릇.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string dish;
+
+int main() {
+	cin >> dish;
+	int suc = 1;
+	int total = 10;
+	for (int i = 1; i < dish.size(); i++) {
+		if (dish[i - 1] == dish[i]) {
+			//successive
+			total+=5;
+		}
+		else {
+			//non-successive
+			total += 10;
+		}
+	}
+	cout << total;
+
+	return 0;
+}
