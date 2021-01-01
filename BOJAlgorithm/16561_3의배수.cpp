0a63d252ae60a7f7b24e689f70e1ece8fe6b9c9a
SuYoung <golborneville@gmail.com>
2021-01-01 20:38:05 +0900

    16561 3의 배수

---
+++ BOJAlgorithm/16561_3의배수.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+bool chk[1003][1003];
+int main() {
+	int n;
+	int k, totalCNT=0;
+	cin >> n;
+	k = n / 3;
+
+	if (k < 3) {
+		cout << 0;
+		return 0;
+	}
+
+	k -= 3;
+	
+	for (int i = 0; i <= k; i++) { // second number
+		for (int j = 0; j <= k; j++) { //first number
+			if (k - i - j < 0) continue;
+
+			if (!chk[i][j]) {
+				chk[i][j] = true;
+				totalCNT++;
+			}
+		}
+	}
+	
+	
+	cout << totalCNT;
+	return 0;
+}
