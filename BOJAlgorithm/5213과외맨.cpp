39979a2c4f37b7199bc30f830dcf7d1952980c53
SuYoung <golborneville@gmail.com>
2021-02-09 15:53:56 +0900

    5213과외맨

--- BOJAlgorithm/5213과외맨.cpp
+++ BOJAlgorithm/5213과외맨.cpp
@@ -1,142 +1,209 @@
 #include<iostream>
+#include<vector>
 #include<cstring>
 #include<queue>
 #include<stack>
 using namespace std;
-//#define INF 987654321 //-> -1로 바꾸자 어차피 +1이니까
-int N;
-int area[505][505];
-pair<int,pair<int,int>> before[505][505];
-int chk[505][505];
-int dx[4] = { 0, -1, 0, 1 };
-int dy[4] = { 1,0, -1,0 };
-queue <pair<int, int>> q;
-stack <int> st; // tile number!!!!!!!
-void getRoad(int x, int y) {
-	int tileNumb = before[x][y].first;
-	int da = x;
-	int db = y;
-	while (tileNumb != 0) {
-		//cout << tileNumb << " ";
-		st.push(tileNumb);
-		int da2 = before[da][db].second.first;
-		int db2 = before[da][db].second.second;
-		tileNumb = before[da2][db2].first;
-		da = da2;
-		db = db2;
-	}
-}
-void BFS(int n1, int n2) {
-	//before[n1][n2] = 1; // 이전단계에 몇번째 타일에서 왔는지 저장
-	chk[n1][n2] = 1;
-	q.push({ n1,n2 });
-	int keep_track = 0;
-	int cnt = 0;
-	int row_before = q.front().first;
-	while (!q.empty()) {
-		pair<int, int> tmp = q.front();
-		q.pop();
+
+int N; //한변 타일 개수
+struct tile {
+	int tileNum;
+	int left;
+	int right;
+	int prev; //이전에 있던 타일의 번호
+};
+int maxtileNum = 1;
+vector<int> tilenum[502];
+tile arr[250003];
+bool chk[250003]; //tile chk 
+int resnum[250003]; //경로....최소 횟수
+queue<pair<int, pair<int, int>>> qu; //타일 번호, {타일이 벡터 안에서i,j 값}
+stack<int> road; //경로 타일 번호!
+void BFS() {
+	//1번 타일부터 탐색 시작 할 예정임.
+	chk[1] = true;
+	//chk[1].second = true;
+
+	while (!qu.empty()) {
+		int tileN = qu.front().first;
+		int vecx = qu.front().second.first;
+		int vecy = qu.front().second.second;
+		qu.pop();
+
+		//오른쪽 왼쪽 체크!
+		//현재 기준 왼쪽 체크
+		if (vecy - 1 >= 0) {
+			if (arr[tilenum[vecx][vecy-1]].right == arr[tileN].left && !chk[tilenum[vecx][vecy-1]]) {
+				//현재 오른쪽 -> 아래 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+				chk[tilenum[vecx][vecy - 1]] = true;
+				arr[tilenum[vecx][vecy - 1]].prev = tileN;
+				qu.push({ tilenum[vecx][vecy - 1],{ vecx, vecy - 1} });
+				if (maxtileNum < tilenum[vecx][vecy - 1])maxtileNum = tilenum[vecx][vecy - 1];
+			}
+		}
 		
-		for (int i = 0; i < 4; i++) {
-			int xx = tmp.first +dx[i];
-			int yy = tmp.second + dy[i];
-			if (xx >= 0 && yy >= 0 && xx < N && yy < 2 * N) {
-				//tmp's tile num == xx yy 's tile num?
-				int tmpTile, xytile;
-				if (tmp.first % 2 == 0) {
-					tmpTile = (tmp.first / 2)*(2 * N - 1) + (tmp.second / 2) + 1;
-				}
-				else {
-					tmpTile = (tmp.first / 2) * (2 * N - 1) + N + ((tmp.second + 1) / 2);
-				}
 
-				if (xx % 2 == 0) {
-					xytile = (xx / 2)*(2 * N - 1) + (yy / 2) + 1;
+		//위 아래 탐색
+		if (vecx % 2 == 0) {
+			//짝수층? 0 2 4...
+			//새 타일은 홀수 층 (N-1개 있음 타일)
+			//현재 타일의 오른쪽체크!-> 홀수 줄 짝수 줄에 따라 달라짐 
+			if (vecy + 1 < N) {
+				if (arr[tilenum[vecx][vecy + 1]].left == arr[tileN].right && !chk[tilenum[vecx][vecy + 1]]) {
+					//현재 오른쪽 -> 아래 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx][vecy + 1]] = true;
+					arr[tilenum[vecx][vecy + 1]].prev = tileN;
+					qu.push({ tilenum[vecx][vecy + 1],{ vecx, vecy + 1 } });
+					if (maxtileNum < tilenum[vecx][vecy + 1])maxtileNum = tilenum[vecx][vecy + 1];
 				}
-				else {
-					xytile = (xx / 2) * (2 * N - 1) + N + ((yy + 1) / 2);
-				}
-				if ((area[tmp.first][tmp.second] == area[xx][yy] || tmpTile == xytile) && chk[xx][yy]==0) {
-					if (tmpTile != xytile) {
-						chk[xx][yy] = chk[tmp.first][tmp.second] + 1;
-						before[xx][yy] = { tmpTile ,{ tmp.first ,tmp.second } };
-					}
-					else {
-						chk[xx][yy] = chk[tmp.first][tmp.second];
-						before[xx][yy] = { before[tmp.first][tmp.second].first ,{ before[tmp.first][tmp.second].second.first, before[tmp.first][tmp.second].second.second } };
+			}
 
-					}
-					q.push({ xx, yy });
-					//마지막 줄 마지막 타일 도착 
-					if (xx == N - 1 && yy == 2 * N - 2) {
-						return;
-					}
+			//오른쪽 기준 탐색 추가 
+			if (vecx + 1 < N && vecy  < N - 1 ){
+				if (arr[tilenum[vecx + 1][vecy]].left == arr[tileN].right && !chk[tilenum[vecx + 1][vecy]]) {
+					//현재 오른쪽 -> 아래 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx + 1][vecy]] = true;
+					arr[tilenum[vecx + 1][vecy]].prev = tileN;
+					qu.push({ tilenum[vecx + 1][vecy],{vecx +1, vecy} });
+					if (maxtileNum < tilenum[vecx + 1][vecy])maxtileNum = tilenum[vecx + 1][vecy];
+				}
+				
+			}
+			if (vecx - 1 >=0 && vecy  < N - 1) {
+				if (arr[tilenum[vecx -1][vecy]].left == arr[tileN].right && !chk[tilenum[vecx - 1][vecy]]) {
+					//현재 오른쪽 -> 위 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx - 1][vecy]] = true;
+					arr[tilenum[vecx - 1][vecy]].prev = tileN;
+					qu.push({ tilenum[vecx - 1][vecy],{ vecx - 1, vecy } });
+					if (maxtileNum < tilenum[vecx - 1][vecy])maxtileNum = tilenum[vecx - 1][vecy];
 				}
 
 			}
+			//왼쪽 기준 탐색 추가
+			if (vecx + 1 < N && vecy-1 >=0 && vecy  < N) {
+				if (arr[tilenum[vecx + 1][vecy - 1]].right == arr[tileN].left && !chk[tilenum[vecx + 1][vecy - 1]]) {
+					//현재 왼쪽 -> 아래 타일의 오른쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx + 1][vecy - 1]] = true;
+					arr[tilenum[vecx + 1][vecy - 1]].prev = tileN;
+					qu.push({ tilenum[vecx + 1][vecy-1],{ vecx + 1, vecy-1 } });
+					if (maxtileNum < tilenum[vecx + 1][vecy - 1])maxtileNum = tilenum[vecx + 1][vecy - 1];
+				}
+			}
+			if (vecx - 1 >= 0 && vecy - 1 >= 0 && vecy < N) {
+				if (arr[tilenum[vecx - 1][vecy - 1]].right == arr[tileN].left && !chk[tilenum[vecx - 1][vecy - 1]]) {
+					//현재 왼쪽 -> 위 타일의 오른쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx - 1][vecy - 1]] = true;
+					arr[tilenum[vecx - 1][vecy - 1]].prev = tileN;
+					qu.push({ tilenum[vecx - 1][vecy-1],{ vecx - 1, vecy-1 } });
+					if (maxtileNum < tilenum[vecx - 1][vecy - 1])maxtileNum = tilenum[vecx - 1][vecy - 1];
+				}
+			}
 		}
-	}
+		//홀수층
+		else if (vecx % 2 != 0) {
 
+			//현재 타일의 오른쪽체크!-> 홀수 줄 짝수 줄에 따라 달라짐 
+			if (vecy + 1 < N-1) {
+				if (arr[tilenum[vecx][vecy + 1]].left == arr[tileN].right && !chk[tilenum[vecx][vecy + 1]]) {
+					//현재 오른쪽 -> 아래 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx][vecy + 1]] = true;
+					arr[tilenum[vecx][vecy + 1]].prev = tileN;
+					qu.push({ tilenum[vecx][vecy + 1],{ vecx, vecy + 1 } });
+					if (maxtileNum < tilenum[vecx][vecy + 1])maxtileNum = tilenum[vecx][vecy + 1];
+				}
+			}
+
+			//오른쪽 기준 탐색 추가 
+			if (vecx + 1 < N) {
+				if (arr[tilenum[vecx + 1][vecy]].right == arr[tileN].left && !chk[tilenum[vecx + 1][vecy]]) {
+					//현재 왼쪽 -> 아래 타일의 오른쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx + 1][vecy]] = true;
+					arr[tilenum[vecx + 1][vecy]].prev = tileN;
+					qu.push({ tilenum[vecx + 1][vecy],{ vecx + 1, vecy } });
+					if (maxtileNum < tilenum[vecx + 1][vecy])maxtileNum = tilenum[vecx + 1][vecy];
+				}
+
+			}
+			if (vecx - 1 >= 0) {
+				if (arr[tilenum[vecx - 1][vecy]].right == arr[tileN].left && !chk[tilenum[vecx - 1][vecy]]) {
+					//현재 왼쪽 -> 위 타일의 오른쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx - 1][vecy]] = true;
+					arr[tilenum[vecx - 1][vecy]].prev = tileN;
+					qu.push({ tilenum[vecx - 1][vecy],{ vecx - 1, vecy } });
+					if (maxtileNum < tilenum[vecx - 1][vecy])maxtileNum = tilenum[vecx - 1][vecy];
+				}
+
+			}
+			//왼쪽 기준 탐색 추가
+			if (vecx - 1 >=0 && vecy + 1 <N) {
+				if (arr[tilenum[vecx - 1][vecy + 1]].left == arr[tileN].right && !chk[tilenum[vecx - 1][vecy + 1]]) {
+					//현재 오른쪽 -> 위 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx - 1][vecy + 1]] = true;
+					arr[tilenum[vecx - 1][vecy + 1]].prev = tileN;
+					qu.push({ tilenum[vecx - 1][vecy + 1],{ vecx - 1, vecy + 1 } });
+					if (maxtileNum < tilenum[vecx - 1][vecy + 1])maxtileNum = tilenum[vecx - 1][vecy + 1];
+				}
+			}
+			if (vecx + 1 <N && vecy + 1 <N) {
+				if (arr[tilenum[vecx + 1][vecy + 1]].left == arr[tileN].right && !chk[tilenum[vecx + 1][vecy + 1]]) {
+					//현재 오른쪽 -> 아래 타일의 왼쪽 이랑 비교 (같은 숫자면 이동가능)
+					chk[tilenum[vecx + 1][vecy + 1]] = true;
+					arr[tilenum[vecx + 1][vecy + 1]].prev = tileN;
+					qu.push({ tilenum[vecx + 1][vecy + 1],{ vecx + 1, vecy + 1 } });
+					if (maxtileNum < tilenum[vecx + 1][vecy + 1])maxtileNum = tilenum[vecx + 1][vecy + 1];
+				}
+			}
+		}
+
+	}
+}
+
+int getWay() {
+	int tile = arr[maxtileNum].tileNum;
+	int cnt = 0;
+	while (arr[tile].tileNum != 0) {
+		road.push(tile);
+		cnt++;
+		tile = arr[tile].prev;
+	}
+	return cnt;
 }
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	memset(area, -1, sizeof(area));
-	memset(chk, 0, sizeof(chk));
-	
+
 	cin >> N;
-	int row = N*N - N / 2;
+	
+	//index 1~ N*N - N/2
+	for (int i = 1; i <= N*N - N/2; i++) {
+		int num1, num2;
+		cin >> num1 >> num2;
+		arr[i] = { i, num1, num2, 0 }; // 일차원 배열로 입력
+	}
+	int ind = 1;
 	for (int i = 0; i < N; i++) {
 		if (i % 2 == 0) {
 			for (int j = 0; j < N; j++) {
-				cin >> area[i][2*j] >> area[i][2 * j +1];
+				tilenum[i].push_back(arr[ind].tileNum);
+				ind++;
 			}
 		}
-		else {
-			//area[i][0] = INF;
-			//area[i][2*N - 1] = INF;
+		else if (i % 2 != 0) {
 			for (int j = 0; j < N-1; j++) {
-				cin >> area[i][2 * j +1] >> area[i][2 * j + 2];
+				tilenum[i].push_back(arr[ind].tileNum);
+				ind++;
 			}
 		}
 	}
-	BFS(0, 1);
-	int inx = N - 1;
-	int iny = 2 * N - 2;  // 마지막 타일의  첫번째 기준으로 탐색하며서 짝 홀 경우 고려X
-	while (chk[inx][iny] == 0) {
-		iny--;
-		if (iny == -1) {
-			iny = 2 * N - 1;
-			inx--;
-		}
-		//if (area[inx][iny] == -1) {
-			//짝수줄이라 옳지못한 접근이라는 의미
-		//	iny = 2 * N - 2;
-		//	inx--;
-		//}
-	}
-	//최대값 최대 타일 번호 지닌 x y 값 나옴
-	//해당 좌표에 대해 이제 경로 추적
-	int sumd;
-	if (inx % 2 == 0) {
-		sumd = (inx / 2)*(2 * N - 1) + (iny / 2) + 1;
-	}
-	else {
-		sumd = (inx / 2) * (2 * N - 1) + N + ((iny + 1) / 2);
+	qu.push({ 1,{0,0} });
+	BFS();
+	int numtile = getWay(); //타일 개수 
+	cout << numtile << "\n";
+	while (!road.empty()) {
+		cout << road.top() << " ";
+		road.pop();
 	}
 
-	cout << chk[inx][iny] << "\n";
-	st.push(sumd);
-	getRoad(inx, iny);
-
-
-
-	while (!st.empty()) {
-		cout << st.top() << " ";
-		st.pop();
-	}
 	return 0;
-
-
 }
